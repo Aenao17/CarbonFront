f52458063cafa5b9e2e063338d261d46641a5d99
Alexandru-Gabriel Oanea <131599239+Aenao17@users.noreply.github.com>
2025-01-12 16:49:32 +0200

    Delete FBackEnd directory

--- FBackEnd/src/main/java/org/example/Domain/Entity.java
+++
@@ -1,6 +0,0 @@
-package org.example.Domain;
-
-public interface Entity<ID> {
-    void setId(ID id);
-    ID getId();
-}
--- FBackEnd/src/main/java/org/example/Service/IObserver.java
+++
@@ -1,7 +0,0 @@
-package org.example.Service;
-
-public interface IObserver {
-
-    // Method that gets called to update the observer with new information or changes
-    void update();
-}
